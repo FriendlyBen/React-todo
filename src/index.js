f63ed499658f2c8c0dfa00dacccd3e5b8b799834
FriendlyBen <bentleyteh2001@gmail.com>
2023-03-29 17:16:41 +0800

    changed things

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter>
+    <BrowserRouter>
       <Routes />
-    </BrowserRouter> */}
-    <App/>
+    </BrowserRouter>
+    {/* <App/> */}
   </React.StrictMode>
 );
 
